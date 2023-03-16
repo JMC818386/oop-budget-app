7d341e87e8f8234f436d7bca193e10a53e7b490b
John-Michael Crawford <jm.crawford@student.awesomeinc.org>
2023-03-16 19:15:30 +0000

    Set deposit description empty string conditional Added "Insufficient Funds" conditional to withdrawal

--- src/category.py
+++ src/category.py
@@ -5,40 +5,43 @@ class Category:
         self.amount = 0
         self.description = ""
 
-    def deposit(self, total, description):
-        self.ledger.append({"Amount": total, "Description": description})
-        self.amount += total
+    def deposit(self, transaction, description = None):
+        if description == None:
+            description = ""
+        self.ledger.append({"Amount": transaction, "Description": description})
+        self.amount += transaction
         print(f'${self.amount}.00 has been deposited into {self.name}\'s account')
-        #print(self.ledger)
-        
-    def withdrawal(self, total, description):
-        self.ledger.append({"Amount": total, "Description": description})
-        self.amount -= total
-        print(f'${self.amount}.00 has been withdrawn from {self.name}\'s account')
         print(self.ledger)
-        #if total == 0
+        
+    def withdrawal(self, transaction, description):
+        self.ledger.append({"Amount": transaction, "Description": description})
+        if transaction > self.amount:
+            print('Insufficent Funds')
+        elif transaction <= self.amount:    
+            self.amount -= transaction
+            print(f'${transaction}.00 has been withdrawn from {self.name}\'s account')
+            print(self.ledger)
             
-
     def get_balance(self):
         print(f'{self.name} has an account balance of ${self.amount}.00.')
 
     def transfer(self):
         pass
 
-    def check_funds(self):
-        print(f'{self.name} has an account balance of ${self.amount}.00.')
+    #def check_funds(self):
+        #print(f'{self.name} has an account balance of ${self.amount}.00.')
 
     #def __str__(self):
         #pass
         
-Jack = Category("Jack")
+jack = Category("Jack")
 food = Category("Food")
 #print(food)
 #print(jm)
 
-Jack.deposit(1000, 'Deposit')
-Jack.withdrawal(50, 'Withdrawal')
-Jack.get_balance()
-Jack.check_funds()
+jack.deposit(1000, 'Deposit')
+jack.withdrawal(1000, 'Withdrawal')
+jack.deposit(500)
+jack.get_balance()
+#Jack.check_funds()
 #food.deposit(22, 'Sandwich')
-
