e572806716ce47f530bbc7c132d12033d52924bd
John-Michael Crawford <jm.crawford@student.awesomeinc.org>
2023-03-16 19:41:50 +0000

    Withdrawl stored as negative int

--- src/category.py
+++ src/category.py
@@ -10,38 +10,45 @@ class Category:
             description = ""
         self.ledger.append({"Amount": transaction, "Description": description})
         self.amount += transaction
-        print(f'${self.amount}.00 has been deposited into {self.name}\'s account')
+        print(f'${self.amount}.00 has been deposited into {self.name}.')
         print(self.ledger)
         
     def withdrawal(self, transaction, description):
-        self.ledger.append({"Amount": transaction, "Description": description})
+        self.ledger.append({"Amount": -abs(transaction), "Description": description})
         if transaction > self.amount:
             print('Insufficent Funds')
         elif transaction <= self.amount:    
             self.amount -= transaction
-            print(f'${transaction}.00 has been withdrawn from {self.name}\'s account')
+            print(f'${transaction}.00 has been withdrawn from the {self.name}.')
             print(self.ledger)
             
     def get_balance(self):
-        print(f'{self.name} has an account balance of ${self.amount}.00.')
+        print(f'{self.name} has a balance of ${self.amount}.00.')
 
     def transfer(self):
         pass
 
     #def check_funds(self):
-        #print(f'{self.name} has an account balance of ${self.amount}.00.')
+        
 
     #def __str__(self):
         #pass
         
-jack = Category("Jack")
+
+rent = Category("Rent")
+utilities = Category("Utilities")
+insurance = Category("Insurance")
 food = Category("Food")
+gas = Category("Gas")
+clothes = Category("Clothes")
+
 #print(food)
 #print(jm)
 
-jack.deposit(1000, 'Deposit')
-jack.withdrawal(1000, 'Withdrawal')
-jack.deposit(500)
-jack.get_balance()
+rent.deposit(2000, 'Deposit')
+rent.withdrawal(1000, 'Withdrawal')
+rent.get_balance()
+
+
 #Jack.check_funds()
 #food.deposit(22, 'Sandwich')
